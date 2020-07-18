266c5a9bd876f1879dcc4468cd08d01ac8713c5b
vamshiN84_username <vamshi.javadev84@gmail.com>
2020-07-18 16:09:42 -0400

    commit

--- src/com/leetcode/practice/FindDuplicateIntegers.java
+++ src/com/leetcode/practice/FindDuplicateIntegers.java
@@ -6,31 +6,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FindDuplicateIntegers {
-	//find duplicate integers in given int[] arrays
+	// find duplicate integers in given int[] arrays
 	public static void main(String[] args) {
-		int[] nums = {1,2,3,1};//define ints
-		boolean isDuplicate = isHavingDuplicates(nums);//call method
+		int[] nums = { 1, 2, 3, 1 };// define ints
+		isHavingDuplicates(nums);// call method
 		isHavingDuplicates1(nums);
-		System.out.println(isDuplicate);//print result
 	}
 
-	private static boolean isHavingDuplicates(int[] nums) {
-		HashSet<Integer> numbers = new HashSet<Integer>();//create an instance for hashset which famous for eliminating duplicates
-		for (int i = 0; i < nums.length; i++) {//loop through all the elements
-			if(numbers.contains(nums[i]))
-			{return true;}//check 
-			numbers.add(i);
-			
+	private static void isHavingDuplicates(int[] nums) {
+		HashSet<Integer> numbers = new HashSet<Integer>();// create an instance for hashset which famous for eliminating
+															// duplicates
+		for (int i = 0; i < nums.length; i++) {// loop through all the elements
+				numbers.add(i);
 		}
-		return false;
-		
-		
+		System.out.println("legacy style" + numbers);
+
 	}
+
 	private static void isHavingDuplicates1(int[] nums) {
 		List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
-		list.parallelStream().collect(Collectors.toSet()).forEach(val->System.out.print(val));
-		
+		list.parallelStream().collect(Collectors.toSet()).forEach(val -> System.out.print(val+","));
 	}
-	
 
 }
